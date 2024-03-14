9686f22d3418b446f238a0fad995ab596ff4f722
BorisMA <borisma1999@yandex.ru>
2024-03-14 22:11:02 +0500

    Change for atmega168

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <avr/io.h>
 #include <stdint.h>
 #include <util/delay.h>
+#include <iom168pa.h>
 
 void main(){
+    DDRB = 1 << DDB5;
     while(1){
+        PORTB ^= 1 << PORT5;
         _delay_ms(200);
-            }
+        }
 }
