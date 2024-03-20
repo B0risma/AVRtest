0d68b6dce98e2afb6ea2381814af7a886f0417cb
BorisovMA <BorisovMA@morion.ru>
2024-03-20 17:52:27 +0500

    some test code

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <avr/io.h>
 #include <stdint.h>
 #include <util/delay.h>
-#include <iom168pa.h>
+#include <avr/iom168pa.h>
+#include <avr/interrupt.h>
 
 
+ISR(INT1_vect){
+    _delay_ms(10);
+}
+
+ISR(TIMER0_OVF_vect){
+    TCNT0 = 0;
+}
 
 void main(){
     DDRB = 1 << DDB5;
